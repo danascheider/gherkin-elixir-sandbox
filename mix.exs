195a1cb386a1e6b90069775df9909b8d47e325a2
Dana Scheider <dana.scheider@gmail.com>
2016-07-22 13:12:16 -0700

    Add package metadata

--- mix.exs
+++ mix.exs
@@ -2,9 +2,13 @@ defmodule Gherkin.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :gherkin_elixir_sandbox,
+    [app: :gherkin,
      version: "0.1.0",
      elixir: "~> 1.3",
+     maintainers: [ "dana.scheider@gmail.com" ],
+     licenses: [ "MIT" ],
+     description: description(),
+     links: %{"GitHub" => "https://github.com/cucumber/gherkin-elixir", "Docs" => "http://cucumber.io"},
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -28,5 +32,11 @@ defmodule Gherkin.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do 
+    """
+    The Elixir implementation of Gherkin.
+    """
   end
 end
