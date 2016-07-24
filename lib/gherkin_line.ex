2d53a1f7c280b452de341030f3cd441d16d6131b
Dana Scheider <dana.scheider@gmail.com>
2016-07-24 11:57:43 -0700

    Make line_text function private

--- lib/gherkin_line.ex
+++ lib/gherkin_line.ex
@@ -1,15 +1,15 @@
 defmodule Gherkin.GherkinLine do
   defstruct text: "", line_number: 1
 
-  def line_text(line) do 
-    line.text
-  end
-
   def trimmed_text(line) do 
     String.trim_leading(line.text)
   end
 
   def indent(line) do 
     String.length(line_text(line)) - String.length(trimmed_text(line))
+  end
+
+  defp line_text(line) do 
+    line.text
   end
 end
--- test/gherkin_line_test.exs
+++ test/gherkin_line_test.exs
@@ -2,12 +2,6 @@ defmodule GherkinLineTest do
   use ExUnit.Case
   doctest Gherkin.GherkinLine
 
-  test ".line_text returns full line text" do 
-    line = %Gherkin.GherkinLine{text: "  Scenario: Foo bar\n", line_number: 3}
-
-    assert Gherkin.GherkinLine.line_text(line) == "  Scenario: Foo bar\n"
-  end
-
   test ".trimmed_text returns trimmed text" do 
     line = %Gherkin.GherkinLine{text: "  Scenario: Foo bar\n", line_number: 3}
 
