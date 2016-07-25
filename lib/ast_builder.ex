8045544e05257223c79554a104954b0bc0d61224
Dana Scheider <dana.scheider@gmail.com>
2016-07-24 18:55:42 -0700

    transform_node\1 for docstring

--- lib/ast_builder.ex
+++ lib/ast_builder.ex
@@ -15,4 +15,18 @@ defmodule Gherkin.AstBuilder do
       text: step_line.matched_text
     }
   end
+
+  def transform_node(ast_node = %Gherkin.AstNode{rule_type: :DocString}) do 
+    {_, separator_token} = Gherkin.AstNode.get_token(ast_node, :DocStringSeparator)
+    content_type         = separator_token.matched_text
+    line_tokens          = Gherkin.AstNode.get_tokens(ast_node, :Other)
+    content              = Enum.map(line_tokens, fn({:Other, t}) -> t.matched_text end) |> Enum.join("\n")
+
+    %{
+      type: ast_node.rule_type,
+      location: Gherkin.Token.get_location(separator_token),
+      content: content,
+      contentType: content_type
+    }
+  end
 end
--- test/ast_builder_test.exs
+++ test/ast_builder_test.exs
@@ -27,4 +27,24 @@ defmodule GherkinAstBuilderTest do
 
     assert Gherkin.AstBuilder.transform_node(ast_node) == expected_output
   end
+
+  test ".transform_node when rule type is :DocString returns appropriate map" do
+    ast_node = %Gherkin.AstNode{
+      rule_type: :DocString,
+      sub_items: [
+        {:DocStringSeparator, %Gherkin.Token{matched_type: :DocString, matched_text: "content_type", location: %{line: 5, column: 9}}},
+        {:Other, %Gherkin.Token{line: %Gherkin.GherkinLine{text: "Something"}, matched_text: "Something"}},
+        {:Other, %Gherkin.Token{line: %Gherkin.GherkinLine{text: "Something Else"}, matched_text: "Something Else"}}
+      ]
+    }
+
+    expected_output = %{
+      type: :DocString,
+      location: %{line: 5, column: 9},
+      contentType: "content_type",
+      content: "Something\nSomething Else"
+    }
+
+    assert Gherkin.AstBuilder.transform_node(ast_node) == expected_output
+  end
 end
