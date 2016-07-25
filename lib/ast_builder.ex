2d29360b6f485c8964b7e455f45d64f40c0ab7d4
Dana Scheider <dana.scheider@gmail.com>
2016-07-24 18:29:30 -0700

    Add one definition for transform_node\1

--- lib/ast_builder.ex
+++ lib/ast_builder.ex
@@ -2,4 +2,17 @@ defmodule Gherkin.AstBuilder do
   def start_rule(stack, rule_type) do
     Enum.concat(stack, [ %Gherkin.AstNode{rule_type: rule_type} ])
   end
+
+  def transform_node(ast_node = %Gherkin.AstNode{rule_type: :Step}) do
+    {_, step_line}  = Gherkin.AstNode.get_token(ast_node, :StepLine)
+    argument        = Gherkin.AstNode.get_token(ast_node, :DataTable) || Gherkin.AstNode.get_token(ast_node, :DocString) || nil
+
+    %{
+      type: ast_node.rule_type,
+      location: Gherkin.Token.get_location(step_line),
+      keyword: step_line.matched_keyword,
+      arguments: argument,
+      text: step_line.matched_text
+    }
+  end
 end
--- test/ast_builder_test.exs
+++ test/ast_builder_test.exs
@@ -7,4 +7,24 @@ defmodule GherkinAstBuilderTest do
 
     assert Gherkin.AstBuilder.start_rule([], :FeatureHeader) == [ast_node]
   end
+
+  test ".transform_node when rule type is :Step returns appropriate map" do 
+    ast_node = %Gherkin.AstNode{
+      rule_type: :Step, 
+      sub_items: [
+        {:StepLine, %Gherkin.Token{matched_type: :StepLine, matched_keyword: "* ", matched_text: "Foo bar", location: %{line: 3, column: 3}}},
+        {:FeatureLine, %Gherkin.Token{matched_type: :FeatureLine}}
+      ]
+    }
+
+    expected_output = %{
+      type: :Step, 
+      location: %{line: 3, column: 3},
+      keyword: "* ",
+      text: "Foo bar",
+      arguments: nil
+    }
+
+    assert Gherkin.AstBuilder.transform_node(ast_node) == expected_output
+  end
 end
