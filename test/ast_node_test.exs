5ed300c59e59b1d5f85becf59b81ec948645c2c1
Dana Scheider <dana.scheider@gmail.com>
2016-07-24 18:29:11 -0700

    Refactor slightly to use structs

--- test/ast_node_test.exs
+++ test/ast_node_test.exs
@@ -4,32 +4,52 @@ defmodule GherkinAstNodeTest do
 
   test ".add adds a child node" do 
     node  = %Gherkin.AstNode{sub_items: [], rule_type: :FeatureHeader}
-    child = {:FeatureLine, {}}
+    child = {:FeatureLine, %Gherkin.Token{}}
 
     assert Gherkin.AstNode.add(node, child) == %Gherkin.AstNode{sub_items: [ child ], rule_type: :FeatureHeader}
   end
 
   test ".get_single returns the correct child" do 
-    node = %Gherkin.AstNode{sub_items: [{:FeatureLine, %{:foo => "bar"}}, {:ScenarioHeader, %{"bar" => "baz"}}, {:ScenarioHeader, %{"baz" => "qux"}}], rule_type: :FeatureHeader}
+    sub_items = [
+      {:FeatureLine, %Gherkin.Token{matched_type: :FeatureLine}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Scenario"}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Foobar"}}
+    ]
+    node      = %Gherkin.AstNode{sub_items: sub_items, rule_type: :FeatureHeader}
 
-    assert Gherkin.AstNode.get_single(node, :ScenarioHeader) == {:ScenarioHeader, %{"bar" => "baz"}}
+    assert Gherkin.AstNode.get_single(node, :ScenarioHeader) == {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Scenario"}}
   end
 
   test ".get_items returns all matching children" do 
-    node = %Gherkin.AstNode{sub_items: [{:FeatureLine, %{:foo => "bar"}}, {:ScenarioHeader, %{"bar" => "baz"}}, {:ScenarioHeader, %{"baz" => "qux"}}], rule_type: :FeatureHeader}
+    [ first, second, third ] = [
+      {:FeatureLine, %Gherkin.Token{matched_type: :FeatureLine}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Scenario"}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Foobar"}}
+    ]
+    node      = %Gherkin.AstNode{sub_items: [first, second, third], rule_type: :FeatureHeader}
 
-    assert Gherkin.AstNode.get_items(node, :ScenarioHeader) == [{:ScenarioHeader, %{"bar" => "baz"}}, {:ScenarioHeader, %{"baz" => "qux"}}]
+    assert Gherkin.AstNode.get_items(node, :ScenarioHeader) == [second, third]
   end
 
-  test ".get_token is the same as .get_single" do 
-    node = %Gherkin.AstNode{sub_items: [{:FeatureLine, %{:foo => "bar"}}, {:ScenarioHeader, %{"bar" => "baz"}}, {:ScenarioHeader, %{"baz" => "qux"}}], rule_type: :FeatureHeader}
+  test ".get_token is the same as .get_single" do
+    sub_items = [
+      {:FeatureLine, %Gherkin.Token{matched_type: :FeatureLine}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Scenario"}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Foobar"}}
+    ]
+    node      = %Gherkin.AstNode{sub_items: sub_items, rule_type: :FeatureHeader}
 
     assert Gherkin.AstNode.get_token(node, :ScenarioHeader) == Gherkin.AstNode.get_single(node, :ScenarioHeader)
   end
 
   test ".get_tokens is the same as .get_items" do 
-    node = %Gherkin.AstNode{sub_items: [{:FeatureLine, %{:foo => "bar"}}, {:ScenarioHeader, %{"bar" => "baz"}}, {:ScenarioHeader, %{"baz" => "qux"}}], rule_type: :FeatureHeader}
+    sub_items = [
+      {:FeatureLine, %Gherkin.Token{matched_type: :FeatureLine}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Scenario"}},
+      {:ScenarioHeader, %Gherkin.Token{matched_type: :ScenarioHeader, matched_keyword: "Foobar"}}
+    ]
+    node      = %Gherkin.AstNode{sub_items: sub_items, rule_type: :FeatureHeader}
 
-    assert Gherkin.AstNode.get_tokens(node, :ScenarioHeader) == Gherkin.AstNode.get_items(node, :ScenarioHeader)
+    assert Gherkin.AstNode.get_items(node, :ScenarioHeader) == Gherkin.AstNode.get_tokens(node, :ScenarioHeader)
   end
 end
