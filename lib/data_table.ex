ec4093de3744445253269f692a8372cb4d951494
Dana Scheider <dana.scheider@gmail.com>
2016-07-25 10:37:35 -0700

    Finish get_table_rows\1 function

--- lib/data_table.ex
+++ lib/data_table.ex
@@ -2,7 +2,7 @@ defmodule Gherkin.DataTable do
   def ensure_cell_count(rows) do 
     count = List.first(rows) |> Map.get(:cells) |> Enum.count
 
-    Enum.each(rows, fn(row) -> raise_error_due_to_cell_count(row, count) end)
+    Enum.each(rows, fn(row) -> raise_error_if_bad_cell_count(row, count) end)
   end
 
   def get_cells(row) do
@@ -15,7 +15,23 @@ defmodule Gherkin.DataTable do
     end)
   end
 
-  defp raise_error_due_to_cell_count(row, count) do
+  def get_table_rows(ast_node) do
+    row_tokens = Gherkin.AstNode.get_tokens(ast_node, :TableRow)
+    
+    rows = Enum.map(row_tokens, fn({_, row}) ->
+      %{
+        type: :TableRow,
+        location: row.location,
+        cells: get_cells(row)
+      }
+    end)
+
+    ensure_cell_count(rows)
+
+    rows
+  end
+
+  defp raise_error_if_bad_cell_count(row, count) do
     if Map.get(row, :cells) |> Enum.count != count do
       loc = Map.get(row, :location)
       raise Gherkin.AstBuilderException, message: "Inconsistent cell count within the table: #{inspect loc}"
--- test/data_table_test.exs
+++ test/data_table_test.exs
@@ -2,7 +2,7 @@ defmodule GherkinDataTableTest do
   use ExUnit.Case
   doctest Gherkin.DataTable
 
-  test ".ensure_cell_count\1 when count is consistent returns nil" do 
+  test ".ensure_cell_count\\1 when count is consistent returns nil" do 
     rows = [
       %{cells: [:foo, :bar]},
       %{cells: [:bar, :baz]},
@@ -12,7 +12,7 @@ defmodule GherkinDataTableTest do
     assert Gherkin.DataTable.ensure_cell_count(rows) == :ok
   end
 
-  test ".ensure_cell_count\1 when count is not consistent raises an error" do 
+  test ".ensure_cell_count\\1 when count is not consistent raises an error" do 
     rows = [
       %{cells: [:foo, :bar]},
       %{cells: [:bar, :baz]},
@@ -26,7 +26,7 @@ defmodule GherkinDataTableTest do
     end)
   end
 
-  test ".get_cells returns the cells in a row" do
+  test ".get_cells\\1 returns the cells in a row" do
     cells  = [
       %Gherkin.Token{matched_type: :TableCell, location: %{line: 4, column: 5}, matched_text: "foo"},
       %Gherkin.Token{matched_type: :TableCell, location: %{line: 4, column: 12}, matched_text: "bar"}
@@ -40,5 +40,40 @@ defmodule GherkinDataTableTest do
     ]
 
     assert Gherkin.DataTable.get_cells(row) == output
+  end
+
+  test ".get_table_rows\\1 returns the rows" do
+    ast_node = %Gherkin.AstNode{
+      rule_type: :DataTable,
+      sub_items: [
+        {:TableRow, %Gherkin.Token{
+          matched_type: :TableRow, 
+          location: %{column: 1, line: 1},
+          matched_items: [%Gherkin.Token{matched_type: :TableCell, location: %{column: 1, line: 1}, matched_text: "foo"}]
+          }
+        },
+        {:TableRow, %Gherkin.Token{
+          matched_type: :TableRow,
+          location: %{column: 1, line: 1},
+          matched_items: [%Gherkin.Token{matched_type: :TableCell, location: %{column: 1, line: 1}, matched_text: "bar"}]
+          }
+        }
+      ]
+    }
+
+    output = [
+      %{
+        cells: [%{location: %{column: 1, line: 1}, type: :TableCell, value: "foo"}],
+        location: %{column: 1, line: 1},
+        type: :TableRow
+      },
+      %{
+        cells: [%{location: %{column: 1, line: 1}, type: :TableCell, value: "bar"}],
+        location: %{column: 1, line: 1},
+        type: :TableRow
+      }
+    ]
+
+    assert Gherkin.DataTable.get_table_rows(ast_node) == output
   end
 end
