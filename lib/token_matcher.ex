63056cd6f06bc440b27f12f2287819f98c065a1d
Dana Scheider <dana.scheider@gmail.com>
2016-07-27 16:49:56 -0700

    .match_tag_line\1 function

--- lib/token_matcher.ex
+++ lib/token_matcher.ex
@@ -1,6 +1,10 @@
 defmodule Gherkin.TokenMatcher do 
   @language_pattern ~r/^\s*#\s*language\s*:\s*([a-zA-Z\-_]+)\s*$/
 
+  def match_tag_line(token) do
+    Gherkin.GherkinLine.starts_with?(token.line, "@")
+  end
+
   def match_title_line(token, token_type, keywords) do
     keyword = Enum.find(keywords, fn(keyword) -> 
       Gherkin.GherkinLine.starts_with_title_keyword?(token.line, keyword)
--- test/token_matcher_test.exs
+++ test/token_matcher_test.exs
@@ -2,6 +2,12 @@ defmodule GherkinTokenMatcherTest do
   use ExUnit.Case
   doctest Gherkin.TokenMatcher
 
+  test ".match_tag_line\\1 when the line doesn't match returns false" do
+    token = %Gherkin.Token{line: %Gherkin.GherkinLine{text: "Foo bar baz"}}
+
+    assert Gherkin.TokenMatcher.match_tag_line(token) == false
+  end
+
   test ".match_title_line\\3 when the line doesn't match returns false" do
     type     = :Step
     token    = %Gherkin.Token{matched_type: type, line: %Gherkin.GherkinLine{text: "Foo bar baz"}}
