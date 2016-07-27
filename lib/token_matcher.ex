a570b169b1f266f92454b34146f5f6a4c9107cba
Dana Scheider <dana.scheider@gmail.com>
2016-07-27 16:45:06 -0700

    Introduce match_title_line\3 function

--- lib/token_matcher.ex
+++ lib/token_matcher.ex
@@ -1,3 +1,15 @@
 defmodule Gherkin.TokenMatcher do 
   @language_pattern ~r/^\s*#\s*language\s*:\s*([a-zA-Z\-_]+)\s*$/
+
+  def match_title_line(token, token_type, keywords) do
+    keyword = Enum.find(keywords, fn(keyword) -> 
+      Gherkin.GherkinLine.starts_with_title_keyword?(token.line, keyword)
+    end)
+
+    if keyword do
+      true
+    else
+      false
+    end
+  end
 end
--- test/token_matcher_test.exs
+++ test/token_matcher_test.exs
@@ -1,4 +1,12 @@
 defmodule GherkinTokenMatcherTest do 
   use ExUnit.Case
   doctest Gherkin.TokenMatcher
+
+  test ".match_title_line\\3 when the line doesn't match returns false" do
+    type     = :Step
+    token    = %Gherkin.Token{matched_type: type, line: %Gherkin.GherkinLine{text: "Foo bar baz"}}
+    keywords = ["Given", "When", "Then"]
+
+    assert Gherkin.TokenMatcher.match_title_line(token, type, keywords) == false
+  end
 end
