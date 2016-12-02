0dbea400c1302ebe185ac8b5a47fc24f718a6f93
Dana Scheider <dana.scheider@gmail.com>
2016-12-02 15:00:39 -0800

    Make dialect stuff work

--- lib/token_matcher.ex
+++ lib/token_matcher.ex
@@ -1,27 +1,38 @@
 defmodule Gherkin.TokenMatcher do
-  def match_tokens([]) do
+  def match_tokens(list, language \\ "en")
+
+  def match_tokens([], language) do
     []
   end
 
-  def match_tokens([head | tail]) do
-     [ match_token(head) ] ++ match_tokens(tail)
+  def match_tokens([head | tail], language) do
+    dialect = match_token(head, language).matched_gherkin_dialect || language
+
+    [ match_token(head, dialect) ] ++ match_tokens(tail, dialect)
   end
 
-  defp match_token(%Gherkin.RawToken{line: %Gherkin.Line{text: nil, line_number: _}, location: _}) do
+  defp match_token(raw_token, language \\ "en")
+
+  defp match_token(%Gherkin.RawToken{line: %Gherkin.Line{text: nil, line_number: num}, location: _}, language) do
     %Gherkin.Token{
-      matched_type: :EOF
+      matched_type: :EOF,
+      matched_indent: 0,
+      matched_gherkin_dialect: language,
+      location: %{line: num, column: 1}
     }
   end
 
-  defp match_token(raw_token) do
+  defp match_token(raw_token, language) do
     cond do
       Gherkin.Line.is_language_header?(raw_token.line) ->
         dialect_name = Regex.run(~r/([a-zA-Z\-_]+)\s*\z/, raw_token.line.text) |> Enum.at(0)
 
         %Gherkin.Token{
           matched_type: :Language,
+          matched_indent: Gherkin.Line.indent(raw_token.line),
           matched_gherkin_dialect: dialect_name,
-          matched_text: dialect_name
+          matched_text: dialect_name,
+          location: %{line: raw_token.line.line_number, column: Gherkin.Line.indent(raw_token.line) + 1}
         }
 
       Gherkin.Line.is_tags?(raw_token.line) ->
@@ -29,7 +40,18 @@ defmodule Gherkin.TokenMatcher do
 
         %Gherkin.Token{
           matched_type: :TagLine,
-          matched_items: tags
+          matched_items: tags,
+          matched_indent: Gherkin.Line.indent(raw_token.line),
+          matched_gherkin_dialect: language,
+          location: %{line: raw_token.line.line_number, column: Gherkin.Line.indent(raw_token.line) + 1}
+        }
+
+      Gherkin.Line.empty?(raw_token.line) ->
+        %Gherkin.Token{
+          matched_type: :Empty,
+          matched_indent: 0,
+          matched_gherkin_dialect: language,
+          location: %{line: raw_token.line.line_number, column: 1}
         }
     end
   end
--- test/token_matcher_test.exs
+++ test/token_matcher_test.exs
@@ -55,7 +55,84 @@ defmodule GherkinTokenMatcherTest do
           %Gherkin.Tag{text: "one", column: 4},
           %Gherkin.Tag{text: "two", column: 9},
           %Gherkin.Tag{text: "three", column: 14}
-        ]
+        ],
+        matched_indent: 3,
+        location: %{line: 1, column: 4}
+      }
+    ]
+
+    assert Gherkin.TokenMatcher.match_tokens(input) == expected_output
+  end
+
+  test ".match_tokens\\1 matches a feature line token" do
+    input           = [
+      %Gherkin.RawToken{
+        location: %{line: 1},
+        line: %Gherkin.Line{text: "Feature: See if this works", line_number: 1}
+      }
+    ]
+
+    expected_output = [
+      %Gherkin.Token{
+        matched_type: :FeatureLine,
+        matched_keyword: "Feature",
+        matched_text: "See if this works",
+        location: %{line: 1, column: 1}
+      }
+    ]
+
+    assert Gherkin.TokenMatcher.match_tokens(input) == expected_output
+  end
+
+  test ".match_tokens\\1 matches an Empty token" do
+    input           = [
+      %Gherkin.RawToken{
+        location: %{line: 1},
+        line: %Gherkin.Line{text: "   ", line_number: 1}
+      }
+    ]
+
+    expected_output = [
+      %Gherkin.Token{
+        matched_type: :Empty,
+        matched_text: nil,
+        location: %{line: 1, column: 1}
+      }
+    ]
+  end
+
+  test ".match_tokens\\1 sets Gherkin dialect correctly" do
+    input           = [
+      %Gherkin.RawToken{
+        location: %{line: 1},
+        line: %Gherkin.Line{text: "# language: it", line_number: 1}
+      },
+      %Gherkin.RawToken{
+        location: %{line: 2},
+        line: %Gherkin.Line{text: "", line_number: 2}
+      },
+      %Gherkin.RawToken{
+        location: %{line: 3},
+        line: %Gherkin.Line{text: nil, line_number: 3}
+      }
+    ]
+
+    expected_output = [
+      %Gherkin.Token{
+        matched_type: :Language,
+        matched_text: "it",
+        matched_gherkin_dialect: "it",
+        location: %{line: 1, column: 1}
+      },
+      %Gherkin.Token{
+        matched_type: :Empty,
+        matched_gherkin_dialect: "it",
+        location: %{line: 2, column: 1}
+      },
+      %Gherkin.Token{
+        matched_type: :EOF,
+        matched_gherkin_dialect: "it",
+        location: %{line: 3, column: 1}
       }
     ]
 
