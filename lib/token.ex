aec885cc0c3a55ba59bf5db773b9a15174e6c8de
Dana Scheider <dana.scheider@gmail.com>
2016-12-02 09:38:03 -0800

    Add location to Token struct

--- lib/token.ex
+++ lib/token.ex
@@ -1,8 +1,9 @@
 defmodule Gherkin.Token do
   defstruct matched_type: :GherkinDocument, 
-            matched_text: "",
-            matched_keyword: "",
+            matched_text: nil,
+            matched_keyword: nil,
             matched_indent: 0,
             matched_items: [],
-            matched_gherkin_dialect: "en"
+            matched_gherkin_dialect: "en",
+            location: %{line: 1, column: 1}
 end
