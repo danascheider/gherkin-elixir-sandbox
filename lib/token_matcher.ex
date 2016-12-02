3fc1fca5fa00b29e9ee1927b37e06481b559ec9c
Dana Scheider <dana.scheider@gmail.com>
2016-12-02 09:58:05 -0800

    Test for matching language token

--- lib/token_matcher.ex
+++ lib/token_matcher.ex
@@ -1,4 +1,6 @@
 defmodule Gherkin.TokenMatcher do
+  @language_pattern ~r/^\s*#\s*language\s*:\s*([a-zA-Z\-_]+)\s*\z/
+
   def match_tokens([]) do
     []
   end
@@ -11,5 +13,19 @@ defmodule Gherkin.TokenMatcher do
     %Gherkin.Token{
       matched_type: :EOF
     }
+  end
+
+  defp match_token(raw_token) do
+    if Regex.match?(@language_pattern, Gherkin.Line.trimmed_text(raw_token.line)) do
+      dialect_name = Regex.run(~r/([a-zA-Z\-_]+)\s*\z/, raw_token.line.text) |> Enum.at(0)
+
+      %Gherkin.Token{
+        matched_type: :Language,
+        matched_gherkin_dialect: dialect_name,
+        matched_text: dialect_name
+      }
+    else
+      # something else
+    end
   end
 end
--- test/token_matcher_test.exs
+++ test/token_matcher_test.exs
@@ -13,11 +13,26 @@ defmodule GherkinTokenMatcherTest do
     expected_output = [
       %Gherkin.Token{
         matched_type: :EOF,
-        matched_text: nil,
-        matched_indent: 0,
-        matched_keyword: nil,
-        matched_items: [],
-        matched_gherkin_dialect: "en",
+        location: %{line: 1, column: 1}
+      }
+    ]
+
+    assert Gherkin.TokenMatcher.match_tokens(input) == expected_output
+  end
+
+  test ".match_tokens\\1 matches language token" do
+    input           = [
+      %Gherkin.RawToken{
+        location: %{line: 1},
+        line: %Gherkin.Line{text: "# language: it", line_number: 1}
+      }
+    ]
+
+    expected_output = [
+      %Gherkin.Token{
+        matched_type: :Language,
+        matched_text: "it",
+        matched_gherkin_dialect: "it",
         location: %{line: 1, column: 1}
       }
     ]
