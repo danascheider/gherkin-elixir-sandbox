e2329ecb6a7bf7e9a137e5074445f95086a1689a
Dana Scheider <dana.scheider@gmail.com>
2016-12-10 12:01:13 -0800

    Make trivial example pass

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,5 +1,8 @@
 defmodule Gherkin.Parser do
   def parse(input, _url) do
-    #
+    %{
+      type: :GherkinDocument,
+      comments: []
+    }
   end
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -2,6 +2,17 @@ defmodule GherkinParserTest do
   use ExUnit.Case
   doctest Gherkin.Parser
 
+  test ".parse\\2 creates a trivial AST" do
+    input           = nil
+
+    expected_output = %{
+      type: :GherkinDocument,
+      comments: []
+    }
+
+    assert Gherkin.Parser.parse(input, "feature.feature") == expected_output
+  end
+
   test ".parse\\2 creates an AST" do
     input           = "Feature: See if this works"
 
